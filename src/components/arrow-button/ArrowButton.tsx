9796a9940a9ffe7c859da8431911af0e0a9bb494
DalilaM25 <melissa25.dm@gmail.com>
2024-07-01 21:27:38 +0300

    функциональность открытия формы

--- src/components/arrow-button/ArrowButton.tsx
+++ src/components/arrow-button/ArrowButton.tsx
@@ -4,16 +4,23 @@ import styles from './ArrowButton.module.scss';
 
 /** Функция для обработки открытия/закрытия формы */
 export type OnClick = () => void;
-
-export const ArrowButton = () => {
+export interface Props {
+	onClick?: OnClick;
+	className?: string | undefined
+}
+export const ArrowButton = (props: Props) => {
+const turnArrow = () => {
+ return `${props.className?.includes('open') ? styles.arrow_open : styles.arrow }`
+}
 	return (
 		/* Не забываем указаывать role и aria-label атрибуты для интерактивных элементов */
 		<div
 			role='button'
 			aria-label='Открыть/Закрыть форму параметров статьи'
 			tabIndex={0}
-			className={styles.container}>
-			<img src={arrow} alt='иконка стрелочки' className={styles.arrow} />
+			className={props.className}
+			onClick={props.onClick}>
+			<img src={arrow} alt='иконка стрелочки' className={turnArrow()} />
 		</div>
 	);
 };
--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,12 +2,32 @@ import { ArrowButton } from 'components/arrow-button';
 import { Button } from 'components/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import arrowStyles from '../arrow-button/ArrowButton.module.scss'
+import { useRef, useState } from 'react';
+import { OnClick } from '../arrow-button/ArrowButton';
+
+type TSetClass = (value:boolean) => string | undefined;
+
 
 export const ArticleParamsForm = () => {
+	const [visibility, setVisibility] = useState(false);
+
+	const toggleVisibility: OnClick = () => {
+		setVisibility(!visibility);
+	}
+
+	const setClass: TSetClass = (visibility) => {
+		return `${styles.container} ${visibility ? styles.container_open : ''}`
+	}
+
+	const setArrowClass: TSetClass =(visibility) => {
+		return `${arrowStyles.container} ${visibility ? arrowStyles.container_open : ''} `
+	}
+
 	return (
 		<>
-			<ArrowButton />
-			<aside className={styles.container}>
+			<ArrowButton className={setArrowClass(visibility)} onClick={toggleVisibility}/>
+			<aside className={setClass(visibility)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' type='reset' />
